eeafa4ea1498cb3927b357ffaabe800de5a85f52
enriquegeorg <enriquegeorg@gmail.com>
2019-05-28 22:28:34 -0300

    Add files via upload

--- tests/circle_test.rb
+++ tests/circle_test.rb
@@ -8,22 +8,22 @@ class TestCircle < Test::Unit::TestCase
   end
 
   def test_area
-    expected_value = 530.92915845671
+    expected_value = 530.92915845668
     assert_equal(expected_value, @circle.area)
 
     @circle.radius = 30
 
-    expected_value = 2827.433388231
+    expected_value = 2827.43338823081
     assert_equal(expected_value, @circle.area)
   end
 
   def test_perimeter
-    expected_value = 81.68140899334
+    expected_value = 81.68140899333
     assert_equal(expected_value, @circle.perimeter)
 
     @circle.radius = 40
 
-    expected_value = 251.3274122872
+    expected_value = 251.32741228718
     assert_equal(expected_value, @circle.perimeter)
   end
 end
--- tests/sphere_test.rb
+++ tests/sphere_test.rb
@@ -8,22 +8,22 @@ class TestSphere < Test::Unit::TestCase
   end
 
   def test_area
-    expected_value = 2123.71663382684
+    expected_value = 2123.7166338267
     assert_equal(expected_value, @sphere.area)
 
     @sphere.radius = 30
 
-    expected_value = 11309.733552924
+    expected_value = 11309.733552923255
     assert_equal(expected_value, @sphere.area)
   end
 
   def test_volume
-    expected_value = 6902.07905993723
+    expected_value = 6902.079059936776
     assert_equal(expected_value, @sphere.volume)
 
     @sphere.radius = 40
 
-    expected_value = 201061.92982976002
+    expected_value = 201061.92982974675
     assert_equal(expected_value, @sphere.volume)
   end
 end
